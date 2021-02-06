d29b300572d3d48e8e884a171580b29572511719
Nico Bazzoni <you@example.com>
2021-02-05 19:52:28 -0500

    commit tree 8128604dd66005e325ef47b545d17799ba1656c8

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
+  
+
+  get '/' do 
+    "Hello, World!"
+  end
+  
   # Define your GET '/' route below and respond with "Hello, World!"
 
 
